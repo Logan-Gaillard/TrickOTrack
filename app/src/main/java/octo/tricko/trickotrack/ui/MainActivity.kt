a43ec8e13f94528969b183ff968e31e700a7cb1d
Logan Gaillard <logangaillard1@gmail.com>
2025-04-25 17:26:47 +0200

    Mise à jour du squelette de l'application

--- app/src/main/java/octo/tricko/trickotrack/ui/MainActivity.kt
+++ app/src/main/java/octo/tricko/trickotrack/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package octo.tricko.trickotrack.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import androidx.core.view.GravityCompat
@@ -10,39 +11,48 @@ import octo.tricko.trickotrack.R
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var navView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout //Déclaration de la variable drawerLayout (= la barre de navigation)
+    private lateinit var navView: NavigationView //Déclaration de la variable navView (= la vue de navigation)
+    private lateinit var navHeader: View //Déclaration de la variable navHeader (= l'en-tête de la vue de navigation)
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState) //Initialisation de l'activité
+        setContentView(R.layout.activity_main) // Définition du layout de l'activité (activity_main.xml)
+        supportFragmentManager.beginTransaction() // Afficher par défaut le fragment AccueilFragment
+            .replace(R.id.fragment_container, AccueilFragment())
+            .commit()
 
-        drawerLayout = findViewById(R.id.drawer_layout)
-        navView = findViewById(R.id.nav_view)
-        val openDrawerButton = findViewById<Button>(R.id.open_drawer_button)
+        drawerLayout = findViewById(R.id.drawer_layout) // Récupération de la référence à la barre de navigation
+        navView = findViewById(R.id.nav_view) // Récupération de la référence à la vue de navigation
+        navHeader = navView.getHeaderView(0) // Récupération de la référence à l'en-tête de la vue de navigation
 
-        openDrawerButton.setOnClickListener {
-            drawerLayout.openDrawer(GravityCompat.START)
+        val openDrawerButton = findViewById<Button>(R.id.open_drawer_button) // Récupération de la référence au bouton d'ouverture de la barre de navigation
+        val closeDrawerButton = navHeader.findViewById<Button>(R.id.close_drawer_button) // Récupération de la référence au bouton de fermeture de la barre de navigation
+
+        openDrawerButton.setOnClickListener { // Action à effectuer lorsque le bouton est cliqué
+            drawerLayout.openDrawer(GravityCompat.START) // Ouverture de la barre de navigation
         }
 
         //Routage des fragments
-        navView.setNavigationItemSelectedListener { menuItem ->
+        navView.setNavigationItemSelectedListener { menuItem -> // Lorsque l'utilisateur sélectionne un élément du menu
             when (menuItem.itemId) {
-                R.id.accueil -> {
-                    // Tu affiches un fragment ou fais une action ici
-                    supportFragmentManager.beginTransaction()
+                R.id.accueil -> { // AccueilFragment
+                    supportFragmentManager.beginTransaction() // Remplace le fragment actuel par AccueilFragment
                         .replace(R.id.fragment_container, AccueilFragment())
                         .commit()
                 }
-                R.id.settings -> {
-                    // Autre action
+                R.id.settings -> { // SettingsFragment
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.fragment_container, SettingsFragment())
                         .commit()
                 }
             }
-            drawerLayout.closeDrawer(GravityCompat.START)
-            true
+            drawerLayout.closeDrawer(GravityCompat.START) // Ferme la barre de navigation
+            true // Indique que l'événement a été traité
+        }
+
+        closeDrawerButton.setOnClickListener {
+            drawerLayout.closeDrawer(GravityCompat.START) // Action à effectuer lorsque le bouton est cliqué
         }
     }
 }
